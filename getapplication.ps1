34505ec4d6078359976619ad9568163d5ad00a9f
vikama-microsoft <36014352+vikama-microsoft@users.noreply.github.com>
2022-06-23 00:31:46 -0700

    Update getapplication.ps1

--- getapplication.ps1
+++ getapplication.ps1
@@ -1,3 +1,5 @@
 param([string] $objectId)
+$output = 'Hello {0}' -f $objectId
+Write-Output $output
 $DeploymentScriptOutputs = @{}
-$DeploymentScriptOutputs['app'] = Get-AzADServicePrincipal -ObjectId $objectId
+$DeploymentScriptOutputs['text'] = $output
